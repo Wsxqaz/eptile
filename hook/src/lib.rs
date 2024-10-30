54a12d202913881f12d176bc7ee81c3192eec5d5
Ryan Hasmatali <14964294+Wsxqaz@users.noreply.github.com>
2024-10-30 00:54:52 -0400

    init hook impl

--- hook/src/lib.rs
+++ hook/src/lib.rs
@@ -3,16 +3,54 @@
 #![feature(lang_items)]
 #![allow(non_camel_case_types, internal_features)]
 
-extern "C" {
-    fn printk(fmt: *const u8, ...) -> i32;
-}
+use core::ffi::*;
 
 #[no_mangle]
-pub extern "C" fn testhook(fn_name: *const u8, fn_hook: extern "C" fn()) -> u64 {
-    unsafe {
-        printk(b"Hooking function:".as_ptr());
-    }
-    5
+extern "C" {
+    fn _printk(s: *const c_char, ...) -> c_int;
 }
 
+
+fn lde_get_length(target: *mut c_void) -> usize {
+    0
+}
+
+fn x86_put_jmp(loc: *mut c_void, target: *mut c_void) {
+    unsafe {
+        let mut offset = target.wrapping_sub(loc as usize).wrapping_sub(5);
+        core::ptr::write(loc as *mut u8, 0xE9);
+        core::ptr::write(loc.wrapping_add(1) as *mut i32, offset as i32);
+    }
+}
+
+struct Hook {
+    target: *mut c_void,
+    replacement: *mut c_void,
+    clone: *mut c_void,
+    original: *mut c_void,
+}
+
+fn hook_fn(target: *mut c_void, replacement: *mut c_void, clone: *mut c_void) -> Hook {
+    unsafe {
+        let mut length = lde_get_length(target);
+        _printk("length: %d".as_ptr() as *const i8, length);
+        while length < 5 {
+            length += lde_get_length(target.wrapping_add(length));
+        }
+        _printk("length: %d".as_ptr() as *const i8, length);
+
+
+        _printk("backing up %d bytes from target (%px) to clone (%px)\n".as_ptr() as *const i8, length, target, clone);
+        core::ptr::copy_nonoverlapping(target, clone, length);
+        _printk("hooking target (%px) to replacement (%px)\n".as_ptr() as *const i8, target, replacement);
+        x86_put_jmp(target, replacement);
+
+        Hook {
+            target,
+            replacement,
+            clone,
+            original: target.wrapping_add(length),
+        }
+    }
+}
 
