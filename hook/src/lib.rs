9af9b36fa65cc1cb6417a1fb84d2b97d34c87168
Ryan Hasmat <ryan.hasmat@gmail.com>
2024-10-31 17:31:24 -0400

    fmt

--- hook/src/lib.rs
+++ hook/src/lib.rs
@@ -14,10 +14,10 @@ extern "C" {
     fn _printk(s: *const c_char, ...) -> c_int;
 }
 
-
 fn lde_get_length(target: *mut c_void) -> i32 {
     unsafe {
-        let mut insn_init: extern "C" fn(*mut insn, *mut c_void, i32, i32) -> c_int = core::mem::transmute(0x0usize);
+        let mut insn_init: extern "C" fn(*mut insn, *mut c_void, i32, i32) -> c_int =
+            core::mem::transmute(0x0usize);
         let mut insn_get_length: extern "C" fn(*mut insn) -> c_int = core::mem::transmute(0x0usize);
 
         let mut insn: insn = core::mem::zeroed();
@@ -51,10 +51,18 @@ fn hook_fn(target: *mut c_void, replacement: *mut c_void, clone: *mut c_void) ->
         }
         _printk("length: %d".as_ptr() as *const i8, length);
 
-
-        _printk("backing up %d bytes from target (%px) to clone (%px)\n".as_ptr() as *const i8, length, target, clone);
+        _printk(
+            "backing up %d bytes from target (%px) to clone (%px)\n".as_ptr() as *const i8,
+            length,
+            target,
+            clone,
+        );
         core::ptr::copy_nonoverlapping(target, clone, length);
-        _printk("hooking target (%px) to replacement (%px)\n".as_ptr() as *const i8, target, replacement);
+        _printk(
+            "hooking target (%px) to replacement (%px)\n".as_ptr() as *const i8,
+            target,
+            replacement,
+        );
         x86_put_jmp(target, replacement);
 
         Hook {
@@ -65,4 +73,3 @@ fn hook_fn(target: *mut c_void, replacement: *mut c_void, clone: *mut c_void) ->
         }
     }
 }
-
