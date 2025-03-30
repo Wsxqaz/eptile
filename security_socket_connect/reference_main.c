b19ed069d4e5a56391256ea29b212d4023df3a6a
Ryan Hasmat <ryan.hasmat@gmail.com>
2025-03-29 22:01:02 -0400

    working vibe coded solution

--- security_socket_connect/reference_main.c
+++ security_socket_connect/reference_main.c
@@ -4,17 +4,21 @@
 #include <linux/init.h>
 #include <linux/stop_machine.h>
 #include <asm/insn.h>
+#include <linux/bpf.h>
+#include <linux/lsm_hooks.h>
+#include <linux/security.h>
 
 long line_to_addr(char *line);
 long find_kallsym(char *name);
 int lde_get_length(const void *p);
 int original_security_socket_connect(struct socket *sock, struct sockaddr *address, int addrlen);
-unsigned int hook_security_socket_connect(struct socket *sock, struct sockaddr *address, int addrlen);
+int hook_security_socket_connect(struct socket *sock, struct sockaddr *address, int addrlen);
 long run_hook(void * addr, int len);
 long write_kernel(void * addr, int len, long (*fn)(void *, int));
-int _run(void *data);
+int __run(void *data);
 long hook_reset(void *data, int len);
 int _hook_reset(void *data);
+void inet_ntoa(struct in_addr in, char *buf);
 int ORIG_LEN = 0;
 
 int * _bpf_prog_active;
@@ -132,92 +136,111 @@ void inet_ntoa(struct in_addr in, char *buf) {
   sprintf(buf, "%d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);
 }
 
-unsigned int hook_security_socket_connect(
+// Define the correct type for socket_connect hook function
+typedef int (*socket_connect_hook_t)(struct socket *sock, struct sockaddr *address, int addrlen);
+
+// Simplified approach - instead of finding the exact hook, we'll directly modify the function
+void *original_hook_addr = NULL;
+
+// Modified hook function that works with BPF LSM - changed return type to int
+int hook_security_socket_connect(
   struct socket *sock,
   struct sockaddr *address,
   int addrlen
 ) {
-  printk(KERN_INFO "hook_security_socket_connect\n");
-  printk(KERN_INFO "sock: %px\n", sock);
-  printk(KERN_INFO "address: %px\n", address);
-  printk(KERN_INFO "address->sa_family: %d\n", address->sa_family);
+    printk(KERN_INFO "hook_security_socket_connect\n");
+    printk(KERN_INFO "sock: %px\n", sock);
+    printk(KERN_INFO "address: %px\n", address);
+    
+    if (address) {
+        printk(KERN_INFO "address->sa_family: %d\n", address->sa_family);
 
-  switch (address->sa_family) {
-    case AF_INET:
-      printk(KERN_INFO "AF_INET\n");
+        switch (address->sa_family) {
+            case AF_INET: {
+                printk(KERN_INFO "AF_INET\n");
 
-      char ip[256];
-      struct sockaddr_in *addr_in = (struct sockaddr_in *)address;
-      inet_ntoa(addr_in->sin_addr, ip);
-      printk(KERN_INFO "ip: %s\n", ip);
+                char ip[256];
+                struct sockaddr_in *addr_in = (struct sockaddr_in *)address;
+                struct in_addr dest_ip = addr_in->sin_addr;
+                inet_ntoa(dest_ip, ip);
+                printk(KERN_INFO "ip: %s\n", ip);
+                
+                // Check if the connection is to 1.1.1.1
+                if (dest_ip.s_addr == htonl(0x01010101)) {  // 1.1.1.1 in network byte order
+                    printk(KERN_INFO "Allowing connection to 1.1.1.1\n");
+                    return 0;  // Allow the connection
+                }
+                
+                break;
+            }
+            case AF_INET6:
+                printk(KERN_INFO "AF_INET6\n");
+                break;
+            case AF_UNIX:
+                printk(KERN_INFO "AF_UNIX\n");
+                break;
+            case AF_UNSPEC:
+                printk(KERN_INFO "AF_UNSPEC\n");
+                break;
+            default:
+                printk(KERN_INFO "default\n");
+                break;
+        }
+    }
 
-      break;
-    case AF_INET6:
-      printk(KERN_INFO "AF_INET6\n");
-      break;
-    case AF_UNIX:
-      printk(KERN_INFO "AF_UNIX\n");
-      break;
-    case AF_UNSPEC:
-      printk(KERN_INFO "AF_UNSPEC\n");
-      break;
-    default:
-      printk(KERN_INFO "default\n");
-      break;
-  }
+    printk(KERN_INFO "addrlen: %d\n", addrlen);
 
-  printk(KERN_INFO "addrlen: %d\n", addrlen);
-
-  return (original_security_socket_connect + ORIG_LEN)(sock, address, addrlen);
+    // Call the original function (which was saved) for all other connections
+    return (original_security_socket_connect + ORIG_LEN)(sock, address, addrlen);
 }
 
 long write_kernel(void * addr, int len, long (*fn)(void *, int)) {
-	long res = 0, cr0, cr4;
+    long res = 0, cr0, cr4;
 
-	asm volatile ("cli\n");
+    asm volatile ("cli\n");
 
-	cr0 = x86_read_cr0();
-	cr4 = x86_read_cr4();
+    cr0 = x86_read_cr0();
+    cr4 = x86_read_cr4();
 
-	if (cr4 & X86_CR4_CET)
-		x86_write_cr4(cr4 & ~X86_CR4_CET);
-	x86_write_cr0(cr0 & ~X86_CR0_WP);
+    if (cr4 & X86_CR4_CET)
+        x86_write_cr4(cr4 & ~X86_CR4_CET);
+    x86_write_cr0(cr0 & ~X86_CR0_WP);
 
-	res = fn(addr, len);
+    res = fn(addr, len);
 
-	x86_write_cr0(cr0);
-	if (cr4 & X86_CR4_CET)
-		x86_write_cr4(cr4);
+    x86_write_cr0(cr0);
+    if (cr4 & X86_CR4_CET)
+        x86_write_cr4(cr4);
 
-	asm volatile ("sti\n");
+    asm volatile ("sti\n");
 
-	return res;
+    return res;
 }
 
+// Override the LSM hook instead of directly hooking the function
+long run_hook(void * addr, int len) {
+    ORIG_LEN = len;
+    original_hook_addr = addr;
 
-long run_hook(void * addr, int len ) {
-  ORIG_LEN = len;
+    // Save the original function implementation
+    memcpy(original_security_socket_connect, addr, len);
+    x86_put_jmp(original_security_socket_connect + len, original_security_socket_connect + len, addr + len);
+    
+    // Debug prints
+    for (int i = 0; i < 20; i++) {
+        printk(KERN_INFO "pre orig [%d]: 0x%02x ", i, ((unsigned char *)original_security_socket_connect)[i]);
+    }
+    
+    // Use direct function hook since we're having trouble with the security hook list
+    printk(KERN_INFO "Using direct function hook\n");
+    x86_put_jmp(addr, addr, hook_security_socket_connect);
+    
+    // Debug prints
+    for (int i = 0; i < 20; i++) {
+        printk(KERN_INFO "addr [%d]: 0x%02x ", i, ((unsigned char *)addr)[i]);
+    }
 
-  for (int i = 0; i < 20; i++) {
-    printk(KERN_INFO "addr [%d]: 0x%02x ", i, ((unsigned char *)addr)[i]);
-  }
-  for (int i = 0; i < 20; i++) {
-    printk(KERN_INFO "pre orig [%d]: 0x%02x ", i, ((unsigned char *)original_security_socket_connect)[i]);
-  }
-
-  memcpy(original_security_socket_connect, addr, len);
-  x86_put_jmp(original_security_socket_connect + len, original_security_socket_connect + len, addr + len);
-
-  for (int i = 0; i < 20; i++) {
-    printk(KERN_INFO "post [%d]: 0x%02x ", i, ((unsigned char *)original_security_socket_connect)[i]);
-  }
-  x86_put_jmp(addr, addr, hook_security_socket_connect);
-
-  for (int i = 0; i < 20; i++) {
-    printk(KERN_INFO "addr [%d]: 0x%02x ", i, ((unsigned char *)addr)[i]);
-  }
-
-  return 0;
+    return 0;
 }
 
 struct args {
@@ -252,28 +275,27 @@ int lde_get_length(const void *p) {
 
 static int driver_entry(void)
 {
-  void * addr = (void *)find_kallsym("security_socket_connect");
-  printk(KERN_INFO "security_socket_connect: %px\n", addr);
+    void * addr = (void *)find_kallsym("security_socket_connect");
+    printk(KERN_INFO "security_socket_connect: %px\n", addr);
 
-  if (addr == 0) {
-    printk(KERN_INFO "security_socket_connect not found\n");
+    if (addr == 0) {
+        printk(KERN_INFO "security_socket_connect not found\n");
+        return 0;
+    }
+
+    int len = lde_get_length((void *)addr);
+    printk(KERN_INFO "security_socket_connect len: %d\n", len);
+    while (len < 5) {
+        len += lde_get_length((void *)(addr + len));
+    }
+    printk(KERN_INFO "security_socket_connect len: %d\n", len);
+    struct args args = {
+        .addr = addr,
+        .len = len
+    };
+    stop_machine(__run, &args, NULL);
+
     return 0;
-  }
-
-
-  int len = lde_get_length((void *)addr);
-  printk(KERN_INFO "security_socket_connect len: %d\n", len);
-  while (len < 5) {
-    len += lde_get_length((void *)(addr + len));
-  }
-  printk(KERN_INFO "security_socket_connect len: %d\n", len);
-  struct args args = {
-    .addr = addr,
-    .len = len
-  };
-  stop_machine(__run, &args, NULL);
-
-  return 0;
 }
 
 int _hook_reset(void *data) {
@@ -282,20 +304,16 @@ int _hook_reset(void *data) {
   return write_kernel(args->addr, args->len, hook_reset);
 }
 
+// Modify hook_reset to restore the original function
 long hook_reset(void *data, int len) {
-  printk(KERN_INFO "trace_call_bpf: %px\n", data);
+    printk(KERN_INFO "Restoring original security hook\n");
 
-  for (int i = 0; i < len; i++) {
-    printk(KERN_INFO "addr [%d]: 0x%02x ", i, ((unsigned char *)data)[i]);
-  }
-  // memcpy(data, original_trace_call_bpf, len);
-  for (int i = 0; i < len; i++) {
-    ((unsigned char *)data)[i] = ((unsigned char *)original_security_socket_connect)[i];
-  }
-  for (int i = 0; i < len; i++) {
-    printk(KERN_INFO "addr [%d]: 0x%02x ", i, ((unsigned char *)data)[i]);
-  }
-  return 0;
+    // Restore the original function bytes
+    for (int i = 0; i < len; i++) {
+        ((unsigned char *)data)[i] = ((unsigned char *)original_security_socket_connect)[i];
+    }
+    
+    return 0;
 }
 
 static void driver_exit(void)
